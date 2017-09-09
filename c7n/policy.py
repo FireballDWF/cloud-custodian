a6a8bb4d0d33e3c6fb93a07ad8c72ca5db5a0300
David Filiatrault <filiatra@amazon.com>
2017-09-08 23:27:56 -0400

    fix expand-variables-v2

--- c7n/policy.py
+++ c7n/policy.py
@@ -474,14 +474,13 @@ class LambdaMode(PolicyExecutionMode):
     def expand_variables(self, variables):
         """expand any variables in the action to_from/cc_from fields.
         """
-        self.policy.log.info("variables=%s", variables)
         p = variables['policy'].copy()
         if 'mode' in variables['policy']:
             if 'role' in variables['policy']['mode']:
                 mode = variables['policy']['mode'].copy()
-                self.policy.log.info("role=%s account_id=%s  formatted=%s", mode['role'], variables['account_id'], mode['role'].format(**variables))
-                # mode['role'] = mode['role'].format(**variables)
-                mode['role'] = mode['role'].format(account_id=variables['account_id'])
+                # self.policy.log.info("role=%s account_id=%s  formatted=%s", mode['role'], variables['account_id'], mode['role'].format(**variables))
+                mode['role'] = mode['role'].format(**variables)
+                # mode['role'] = mode['role'].format(account_id=variables['account_id'])
                 self.policy.log.info("mode=%s role=%s", mode, mode['role'])
                 p['mode'] = mode
         return p
